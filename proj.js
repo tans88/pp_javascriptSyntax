a6d74f3cbd44c345da86f8a8d4b29ad23572d6a1
Tanya Rath <emailtanya8@gmail.com>
2024-07-17 14:23:30 -0700

    project

--- proj.js
+++ proj.js
@@ -1,4 +1,11 @@
 import fetch from 'node-fetch';
+import readline from 'readline'
+
+// create an instance of readline.Interface
+const rl = readline.createInterface({
+    input: process.stdin,   // reads input 
+    output: process.stdout  // outputs text to console
+});
 
 // function to retrieve random space fact
 async function getRandomSpaceFact(){
@@ -9,11 +16,34 @@ async function getRandomSpaceFact(){
     const randomIndex = Math.floor(Math.random() * data.bodies.length);
     const randomBody = data.bodies[randomIndex];
 
-    // display info 
+    // display info
+    console.log();
+    console.log();
+    console.log("Your random celestial body fact!")
     console.log(`Name: ${randomBody.englishName}`);
     console.log(`Type: ${randomBody.bodyType}`);
     console.log(`Density: ${randomBody.density}`);
     console.log(`Gravity: ${randomBody.gravity}`);
+    getUserInput();
 }
 
-getRandomSpaceFact();
+// function to prompt user input
+function getUserInput(){
+    // prompt user for question using rl instance 
+    rl.question("Enter: ", (answer) => {
+        if (answer.toLowerCase() == "yes"){
+            getRandomSpaceFact();
+        } else if (answer.toLowerCase() == "stop"){
+            console.log("Goodbye!");
+            rl.close();
+        } else{
+            console.log("Invalid input. Please type 'yes' or 'stop'.");
+            getUserInput();
+        }
+    });
+}
+
+console.log("Hello and welcome to the random celestial body generator!");
+console.log("A celestial body is a natural object that exists in space. This includes planets, moons, asteroids, stars, comets, galaxies, and more!");
+console.log("To generate a random celestial body, type 'yes.' If you want to escape, type 'stop.'");
+getUserInput();
